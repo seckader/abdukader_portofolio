3fd1da21bcb7682c29c0e3a0fc88133f9c8fce2a
abdukader <seckader.pro@gmail.com>
2026-05-05 22:43:02 -0400

    [ak-0019][fix] - add blog fragment to back link navigation

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -20,6 +20,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled',
     scrollPositionRestoration: 'enabled',
   })],
   exports: [RouterModule]
